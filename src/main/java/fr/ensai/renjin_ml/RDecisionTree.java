1c5b0b06653d04598b09c8a4b9862b4fbd92a12c
alka <alka@ZBook-19>
2018-01-09 11:49:09 +0100

    Renjin descision tree part1

---
+++ src/main/java/fr/ensai/renjin_ml/RDecisionTree.java
@@ -0,0 +1,22 @@
+package fr.ensai.renjin_ml;
+
+//Import des packages pour l'évaluation des expressions R
+import org.renjin.script.RenjinScriptEngine;
+import org.renjin.script.RenjinScriptEngineFactory;
+
+public class RDecisionTree {
+
+	/*
+	 * Defining the Renjin 
+	 */
+	// create a script engine manager:
+    RenjinScriptEngineFactory factory = new RenjinScriptEngineFactory();
+    // create a Renjin engine:
+    RenjinScriptEngine engine = factory.getScriptEngine();
+    
+	
+	public RDecisionTree() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
