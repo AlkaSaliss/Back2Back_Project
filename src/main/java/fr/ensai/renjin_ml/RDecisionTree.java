e4fa4b7f073bea8e398b4789f143a8156e815531
alka <alka@ZBook-19>
2018-01-16 14:16:46 +0100

    Version semi-définitive de Renjine

--- src/main/java/fr/ensai/renjin_ml/RDecisionTree.java
+++ src/main/java/fr/ensai/renjin_ml/RDecisionTree.java
@@ -18,7 +18,7 @@ public class RDecisionTree {
 	private String formula;
 	//private String dataPath;
 	private String method;
-	private String col_Y; //column number of y in the dataset 
+	private int col_Y; //column number of y in the dataset 
 	
 	private RenjinScriptEngineFactory factory = new RenjinScriptEngineFactory();
     // create a Renjin engine:
@@ -29,12 +29,13 @@ public class RDecisionTree {
 	
 	
 	
-	public RDecisionTree(String form, String method, String coly) {
+	public RDecisionTree(String form, String method, int coly) {
 		
 		this.formula = form;
 		//this.dataPath = data;
 		this.method = method;
 		this.col_Y=coly;
+		
 		
 	}
 	
@@ -42,15 +43,21 @@ public class RDecisionTree {
 		/*
 		 * loading the file
 		 */
-		engine.eval("data <- read.csv("+dataPath+", header=T, row.names=1)");
+		engine.eval("data <- as.data.frame(read.csv("+dataPath+", header=T, row.names=1))");
 		/*
 		 * Spliting data into training and test sets
 		 */
+		//Shuffle the data rows
+		engine.eval("set.seed(123)");
+		engine.eval("data <- data[sample(1:nrow(data)), ]");
+		
 		engine.eval("n=2*floor(nrow(data)/3)");
 		engine.eval("train=data[1:n,]");
-		engine.eval("library(rpart)"); //load the package
+		
+		//load the package for decision tree modeling
+		engine.eval("library(rpart)"); 
 		engine.eval("res <- rpart("+this.formula + ",data=train)");
-		engine.eval("print(res)");
+		
 		
 	}
 	
@@ -61,20 +68,28 @@ public class RDecisionTree {
 		
 		//TODO
 		
-		engine.eval("test <-na.omit(data[n+1:nrow(data),])");
-		engine.eval("print(typeof(train))");
-		engine.eval("print(typeof(test))");
-		
-		
-		
-		
+		engine.eval("test <- na.omit(data[n+1:nrow(data),])");
 		/*
 		 * Prediction
 		 */
-		engine.eval("pred<-predict(res, train)");
-		/*engine.eval("confMat <- table(test[,"+this.col_Y+"], pred)");
+		engine.eval("pred <- predict(res, test, type=\"class\")");
+		//engine.eval("print(test[,"+this.col_Y+"])");
+		//engine.eval("print(pred)");
+		engine.eval("confMat <- table(test[,"+this.col_Y+"], pred)");
 		engine.eval("accuracy <- sum(diag(confMat))/sum(confMat)");
-		System.out.println("The tree accuracy is : " + engine.get("accuracy"));*/
+		System.out.println("The tree accuracy is : " + engine.get("accuracy"));
+		System.out.println("******************************************\n"
+				+ "******************************************\n"
+				+ "******************************************\n");
+		
+		engine.eval("library(partykit)");
+		engine.eval("res2 <- as.party(res)");
+		engine.eval("print(res2)");
+		/*engine.eval("jpeg('/src/main/resources/treePlot.jpg')");
+		engine.eval("plot(res2)");
+		engine.eval("dev.off()");*/
+		
+		//engine.eval("plot(res, uniform=TRUE, branch=0.6, margin=0.05)");
 		
 	}
 	
@@ -83,18 +98,18 @@ public class RDecisionTree {
 	public static void main(String[] args) throws Exception {
 	   
 		//create the tree object
-		RDecisionTree tree = new RDecisionTree("Species~.", "class", "5");
+		RDecisionTree tree = new RDecisionTree("Species~.", "class", 5);
 		tree.fit("\"src/main/resources/iris.csv\"");
 		tree.predict();
 		
 		
 		// create a script engine manager:
-	    //RenjinScriptEngineFactory factory = new RenjinScriptEngineFactory();
+	   /* RenjinScriptEngineFactory factory = new RenjinScriptEngineFactory();
 	    // create a Renjin engine:
-	    //RenjinScriptEngine engine = factory.getScriptEngine();
+	    RenjinScriptEngine engine = factory.getScriptEngine();
 
-	/*    engine.eval("dat <- data.frame(x=1:10, y=(1:10)+rnorm(n=10))");
-	    engine.eval("print(dat)");*/
+	    engine.eval("dat <- data.frame(x=1:10, y=(1:10)+rnorm(n=10))");
+	    engine.eval("print(typeof(dat))");*/
 	    
 	 /*   String chemin = "\"src/main/resources/iris.csv\"";
 	    engine.eval("data <- read.csv(" + chemin + ", header=T, row.names=1)");*/
