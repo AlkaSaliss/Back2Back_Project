221012017eb441a83becb4bdc2856d63af5c73d2
naomigi <naomi.girard@gmail.com>
2018-01-23 14:13:09 +0100

    spaarkml first test

---
+++ main/SparkSession.java
@@ -0,0 +1,61 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
+//public class SparkSession
+//{
+//	
+//	private SparkConf sparkConf;
+//	private JavaSparkContext sc;
+//	
+//    /** Constructeur privé */
+//    private SparkSession(String name)
+//    {
+//    	sparkConf = new SparkConf().setAppName(name).setMaster("local");
+//    	sc = new JavaSparkContext(sparkConf);
+//    }
+//     
+//    /** Instance unique non préinitialisée */
+//    private static SparkSession INSTANCE = null;
+//     
+//    /** Point d'accès pour l'instance unique du singleton */
+//    public static SparkSession getInstance()
+//    {           
+//        if (INSTANCE == null)
+//        {   INSTANCE = new SparkSession("SparkSession"); 
+//        }
+//        return INSTANCE;
+//    }
+//}
+
+
+public class SparkSession
+{       
+	
+	private JavaSparkContext sc;
+	
+    /** Constructeur privé */  
+    private SparkSession(String name)
+    {
+    	SparkConf sparkConf = new SparkConf().setAppName(name).setMaster("local");
+    	sc = new JavaSparkContext(sparkConf);
+    }
+     
+
+	public JavaSparkContext getSc() {
+		return sc;
+	}
+
+	
+    /** Holder */
+    private static class SingletonHolder
+    {       
+        /** Instance unique non préinitialisée */
+        private final static SparkSession instance = new SparkSession("SparkSession");
+    }
+ 
+    /** Point d'accès pour l'instance unique du singleton */
+    public static SparkSession getInstance()
+    {
+        return SingletonHolder.instance;
+    }
+}
